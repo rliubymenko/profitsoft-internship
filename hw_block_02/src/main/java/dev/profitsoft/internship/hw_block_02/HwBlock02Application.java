e9eb1d8338c849be9e95b5a50c53accd4be48dd3
rliubymenko <rliubymenko@gmail.com>
2024-05-10 20:04:44 +0200

    add hw_block_02

---
+++ hw_block_02/src/main/java/dev/profitsoft/internship/hw_block_02/HwBlock02Application.java
@@ -0,0 +1,13 @@
+package dev.profitsoft.internship.hw_block_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwBlock02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwBlock02Application.class, args);
+	}
+
+}
---
+++ hw_block_02/src/test/java/dev/profitsoft/internship/hw_block_02/HwBlock02ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.profitsoft.internship.hw_block_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwBlock02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
