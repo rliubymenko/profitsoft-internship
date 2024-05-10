1abcd4969de30768cc3d1743cdfe822c4a9ce84f
rliubymenko <rliubymenko@gmail.com>
2024-05-10 20:07:15 +0200

    change hw_block_02 onto maven

--- hw_block_02/src/main/java/dev/profitsoft/internship/hw_block_02/HwBlock02Application.java
+++ hw_block_02/src/main/java/dev/profitsoft/internship/hw_block_02/HwBlock02Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HwBlock02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HwBlock02Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HwBlock02Application.class, args);
+    }
 
 }
--- hw_block_02/src/test/java/dev/profitsoft/internship/hw_block_02/HwBlock02ApplicationTests.java
+++ hw_block_02/src/test/java/dev/profitsoft/internship/hw_block_02/HwBlock02ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HwBlock02ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
