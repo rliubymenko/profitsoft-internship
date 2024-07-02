a299568a467ad218be6ea0d07227d98d72478473
rliubymenko <rliubymenko@gmail.com>
2024-07-02 20:07:13 +0200

    add back-end url to front as an env variable

--- hw_block_03/src/config/index.js
+++ hw_block_03/src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     // Services
-    USERS_SERVICE: 'http://localhost:8080/api/v1',
+    USERS_SERVICE: process.env.USERS_SERVICE || '',
     UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
 
