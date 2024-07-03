a00758c1a60579bee867fa101d76a5aa05d0f735
rliubymenko <rliubymenko@gmail.com>
2024-07-03 11:41:55 +0200

    change REACT_APP_BACK_SERVICE

--- hw_block_03/src/config/index.js
+++ hw_block_03/src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     // Services
-    USERS_SERVICE: process.env.USERS_SERVICE || '',
+    USERS_SERVICE: process.env.REACT_APP_BACK_SERVICE || '',
     UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
 
