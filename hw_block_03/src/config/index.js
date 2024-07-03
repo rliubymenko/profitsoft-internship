d8ffb9793331172f73848b5391dfda577bddc298
rliubymenko <rliubymenko@gmail.com>
2024-07-03 17:39:44 +0200

    change location of back

--- hw_block_03/src/config/index.js
+++ hw_block_03/src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     // Services
-    USERS_SERVICE: process.env.REACT_APP_BACK_SERVICE || '',
+    USERS_SERVICE: process.env.REACT_APP_BACK_SERVICE ? `${process.env.REACT_APP_BACK_SERVICE}/api/v1` : '',
     UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
 
